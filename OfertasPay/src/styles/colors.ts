dd4fc30439f11dc630bbe05a246d2729a05a3349
Juan Pablo <jpablo.dev@gmail.com>
2025-03-17 18:28:23 -0300

    Criação e implementação de estilizantes

---
+++ OfertasPay/src/styles/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+    gray:{
+        100: "#FCFDFE",
+        200: "#E1EBF4",
+        300: "#C4D0DB",
+        400: "#73808C",
+        500: "#45524F",
+        600: "#1A1F24",  
+    },
+
+    blue:{
+        soft: "#E9F3EF",
+        light: "#7CAFE2",
+        base: "##5096DC",
+        dark: "#156FC8",
+    },
+
+    red:{
+        light: "#FDEDED",
+        base: "#F94144",
+    },
+}
